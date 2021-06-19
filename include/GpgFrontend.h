e9dd67c7452594f8f9ae75a86a4d769a087980a1
Saturneric <eric.bktu@gmail.com>
2021-06-19 19:53:37 +0800

    Make some corrections.

--- include/GpgFrontend.h
+++ include/GpgFrontend.h
@@ -17,16 +17,16 @@
 #include <gpgme.h>
 
 #define PROJECT_NAME "GpgFrontend"
-#define BUILD_VERSION "1.0.0_Windows-10.0.19042_AMD64_Debug"
-#define GIT_VERSION "main_1b75d7882b8a1fbd5b61424060c7ac81a1ea9fe4"
+#define BUILD_VERSION "1.0.0_Windows-10.0.19042_AMD64_Release"
+#define GIT_VERSION "main_40c3cb1c86dbaf4bc0f3c592e9d733dc1768423b"
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
 
-#define BUILD_TIMESTAMP "2021-06-19 19:47:51"
+#define BUILD_TIMESTAMP "2021-06-19 19:53:15"
 
 #define GIT_BRANCH_NAME "main"
-#define GIT_COMMIT_HASH "1b75d7882b8a1fbd5b61424060c7ac81a1ea9fe4"
+#define GIT_COMMIT_HASH "40c3cb1c86dbaf4bc0f3c592e9d733dc1768423b"
 
 #endif //GPGFRONTEND_H_IN
