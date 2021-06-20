a02379564a10d6657d6aadf357ed1cc6302f6f16
Saturneric <eric.bktu@gmail.com>
2021-06-20 17:10:31 +0800

    Update cmake.yml Fix CMakeLists.txt

--- include/GpgFrontend.h
+++ include/GpgFrontend.h
@@ -18,15 +18,15 @@
 
 #define PROJECT_NAME "GpgFrontend"
 #define BUILD_VERSION "1.0.0_Windows-10.0.19042_AMD64_Release"
-#define GIT_VERSION "main_ecd0e478c9a84d1d750a827bf80e3ca2ec97adc9"
+#define GIT_VERSION "main_987b04aac3bae8779c7b0af0dae7bd3f8a9047d3"
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
 
-#define BUILD_TIMESTAMP "2021-06-20 16:06:56"
+#define BUILD_TIMESTAMP "2021-06-20 16:54:59"
 
 #define GIT_BRANCH_NAME "main"
-#define GIT_COMMIT_HASH "ecd0e478c9a84d1d750a827bf80e3ca2ec97adc9"
+#define GIT_COMMIT_HASH "987b04aac3bae8779c7b0af0dae7bd3f8a9047d3"
 
 #endif //GPGFRONTEND_H_IN
