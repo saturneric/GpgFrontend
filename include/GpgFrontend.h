3c9620153021902b189c3117649b5f0e5a7c9f7f
Saturneric <eric.bktu@gmail.com>
2021-06-20 18:41:29 +0800

    Fix cmake.yml For Windows Build.

--- include/GpgFrontend.h
+++ include/GpgFrontend.h
@@ -18,15 +18,15 @@
 
 #define PROJECT_NAME "GpgFrontend"
 #define BUILD_VERSION "1.0.0_Windows-10.0.19042_AMD64_Release"
-#define GIT_VERSION "main_987b04aac3bae8779c7b0af0dae7bd3f8a9047d3"
+#define GIT_VERSION "main_2104aa97267bd755f9fdffbf35f472929418de6b"
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
 
-#define BUILD_TIMESTAMP "2021-06-20 16:54:59"
+#define BUILD_TIMESTAMP "2021-06-20 18:28:34"
 
 #define GIT_BRANCH_NAME "main"
-#define GIT_COMMIT_HASH "987b04aac3bae8779c7b0af0dae7bd3f8a9047d3"
+#define GIT_COMMIT_HASH "2104aa97267bd755f9fdffbf35f472929418de6b"
 
 #endif //GPGFRONTEND_H_IN
