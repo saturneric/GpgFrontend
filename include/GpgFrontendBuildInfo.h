965d41924bb4f98d383e09d2848a2972bc5e03a1
Saturneric <eric.bktu@gmail.com>
2021-07-21 00:39:52 +0800

    Update Workflow

---
+++ include/GpgFrontendBuildInfo.h
@@ -0,0 +1,44 @@
+/**
+ * This file is part of GPGFrontend.
+ *
+ * GPGFrontend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * The initial version of the source code is inherited from gpg4usb-team.
+ * Their source code version also complies with GNU General Public License.
+ *
+ * The source code version of this software was modified and released
+ * by Saturneric<eric@bktus.com> starting on May 12, 2021.
+ *
+ */
+
+#ifndef GPGFRONTEND_BUILD_INFO_H_IN
+#define GPGFRONTEND_BUILD_INFO_H_IN
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 2
+#define VERSION_PATCH 2
+
+#define GIT_BRANCH_NAME "develop"
+#define GIT_COMMIT_HASH "70053b17c76b010537a5c183621dc0c14e9e416e"
+
+#define PROJECT_NAME "GpgFrontend"
+#define BUILD_VERSION "1.2.2_Darwin-20.5.0_x86_64_Release"
+#define GIT_VERSION "develop_70053b17c76b010537a5c183621dc0c14e9e416e"
+#define OS_PLATFORM 1
+
+#define BUILD_FLAG 0
+
+#define BUILD_TIMESTAMP "2021-07-20 21:56:45"
+
+#endif // GPGFRONTEND_BUILD_INFO_H_IN
