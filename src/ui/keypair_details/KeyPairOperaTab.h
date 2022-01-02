fbb9123050352b5f252cd32919301987c89855e7
Saturneric <eric@bktus.com>
2022-01-02 17:12:00 +0800

    <refactor>(ui): take operations apart from key details.

--- src/ui/keypair_details/KeyPairOperaTab.h
+++ src/ui/keypair_details/KeyPairOperaTab.h
@@ -18,27 +18,52 @@
  * Their source code version also complies with GNU General Public License.
  *
  * The source code version of this software was modified and released
- * by Saturneric<eric@bktus.com> starting on May 12, 2021.
+ * by Saturneric<eric@bktus.com><eric@bktus.com> starting on May 12, 2021.
  *
  */
 
-#ifndef __KEYDETAILSDIALOG_H__
-#define __KEYDETAILSDIALOG_H__
+#ifndef GPGFRONTEND_KEYPAIROPERATAB_H
+#define GPGFRONTEND_KEYPAIROPERATAB_H
 
-#include "gpg/GpgContext.h"
+#include "gpg/function/GpgKeyGetter.h"
 #include "ui/GpgFrontendUI.h"
 
 namespace GpgFrontend::UI {
-
-class KeyDetailsDialog : public QDialog {
+class KeyPairOperaTab : public QWidget {
   Q_OBJECT
-
  public:
-  explicit KeyDetailsDialog(const GpgKey& key, QWidget* parent = nullptr);
+  KeyPairOperaTab(const std::string& key_id, QWidget* parent);
+
+  void createOperaMenu();
+
+ private slots:
+
+  /**
+   * @details Export the key to a file, which is chosen in a file dialog
+   */
+  void slotExportPrivateKey();
+
+  void slotExportShortPrivateKey();
+
+  void slotExportPublicKey();
+
+  void slotModifyEditDatetime();
+
+  void slotModifyPassword();
+
+  void slotUploadKeyToServer();
+
+  void slotUpdateKeyFromServer();
+
+  void slotGenRevokeCert();
+
+  void slotModifyTOFUPolicy();
 
  private:
-  QTabWidget* tabWidget{};
+  GpgKey m_key_;
+  QMenu* keyServerOperaMenu{};
+  QMenu* secretKeyExportOperaMenu{};
 };
 }  // namespace GpgFrontend::UI
 
-#endif  // __KEYDETAILSDIALOG_H__
+#endif  // GPGFRONTEND_KEYPAIROPERATAB_H
