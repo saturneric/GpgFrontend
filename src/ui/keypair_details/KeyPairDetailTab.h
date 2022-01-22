036bbfbe8ed09a4f4d09a37a3a002bb8ec0aecf6
Saturneric <eric@bktus.com>
2022-01-23 04:11:32 +0800

    <license, style>(src, test): Fix some issues in license statement.

--- src/ui/keypair_details/KeyPairDetailTab.h
+++ src/ui/keypair_details/KeyPairDetailTab.h
@@ -1,4 +1,6 @@
 /**
+ * Copyright (C) 2021 Saturneric
+ *
  * This file is part of GpgFrontend.
  *
  * GpgFrontend is free software: you can redistribute it and/or modify
@@ -6,16 +8,16 @@
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * GpgFrontend is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ * along with GpgFrontend. If not, see <https://www.gnu.org/licenses/>.
  *
- * The initial version of the source code is inherited from gpg4usb-team.
- * Their source code version also complies with GNU General Public License.
+ * The initial version of the source code is inherited from
+ * the gpg4usb project, which is under GPL-3.0-or-later.
  *
  * The source code version of this software was modified and released
  * by Saturneric<eric@bktus.com><eric@bktus.com> starting on May 12, 2021.
@@ -85,7 +87,6 @@ class KeyPairDetailTab : public QWidget {
   QMenu* secretKeyExportOperaMenu{};  ///<
 
  public:
-  
   /**
    * @brief Construct a new Key Pair Detail Tab object
    *
