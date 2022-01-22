c385a328a5b820588510816977b3b96ebc5b11e2
Saturneric <eric@bktus.com>
2022-01-23 04:14:00 +0800

    <license, style>(src): Modify the license statement.
    1. Modify the original license statement in the header of the HelpPage.h file.

--- src/ui/widgets/HelpPage.h
+++ src/ui/widgets/HelpPage.h
@@ -1,22 +1,29 @@
-/*
- *      helppage.h
+/**
+ * Copyright (C) 2021 Saturneric
  *
- *      Copyright 2008 gpg4usb-team <gpg4usb@cpunk.de>
+ * This file is part of GpgFrontend.
  *
- *      This file is part of gpg4usb.
+ * GpgFrontend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *      Gpg4usb is free software: you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation, either version 3 of the License, or
- *      (at your option) any later version.
+ * GpgFrontend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *      Gpg4usb is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with GpgFrontend. If not, see <https://www.gnu.org/licenses/>.
  *
- *      You should have received a copy of the GNU General Public License
- *      along with gpg4usb.  If not, see <http://www.gnu.org/licenses/>
+ * The initial version of the source code is inherited from
+ * the gpg4usb project, which is under GPL-3.0-or-later.
+ *
+ * All the source code of GpgFrontend was modified and released by
+ * Saturneric<eric@bktus.com> starting on May 12, 2021.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
  */
 
 #ifndef HELPPAGE_H
