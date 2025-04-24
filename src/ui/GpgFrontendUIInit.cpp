0ef4b47994487bf57be7e102a34923a4f494d0fe
saturneric <eric@bktus.com>
2025-04-24 19:39:50 +0200

    fix: tool tip base should be black

--- src/ui/GpgFrontendUIInit.cpp
+++ src/ui/GpgFrontendUIInit.cpp
@@ -151,7 +151,7 @@ void InitGpgFrontendUI(QApplication* app) {
     dark_palette.setColor(QPalette::WindowText, Qt::white);
     dark_palette.setColor(QPalette::Base, QColor(25, 25, 25));
     dark_palette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
-    dark_palette.setColor(QPalette::ToolTipBase, Qt::white);
+    dark_palette.setColor(QPalette::ToolTipBase, Qt::black);
     dark_palette.setColor(QPalette::ToolTipText, Qt::white);
     dark_palette.setColor(QPalette::Text, Qt::white);
     dark_palette.setColor(QPalette::Button, QColor(53, 53, 53));
